da8ea835e8471bce8dd3c4ebcba8bcecafb81c04
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2023-09-17 00:17:05 +0200

    fix for 429? Too Many Requests

--- TG-Sticker-Resizer-Bot.py
+++ TG-Sticker-Resizer-Bot.py
@@ -1,9 +1,23 @@
 import telebot
 from PIL import Image
 import os
+import time
 
-TOKEN = "YOUR_BOT_TOKEN_HERE"
+TOKEN = "6400495210:AAFmAqWPnbkfqI8e6AjrCUHTUE8lixqi0rs"
 bot = telebot.TeleBot(TOKEN)
+
+def retry_after(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except telebot.apihelper.ApiTelegramException as e:
+            if e.error_code == 429:
+                wait_time = e.result_json.get('parameters', {}).get('retry_after', 5)
+                print(f"Too many requests. Waiting {wait_time} seconds.")
+                time.sleep(wait_time)
+                return wrapper(*args, **kwargs)
+            raise e
+    return wrapper
 
 def resize_and_convert_image(input_path, output_path):
     with Image.open(input_path) as image:
@@ -16,13 +30,14 @@ def resize_and_convert_image(input_path, output_path):
             bottom_margin = (image.height + 512) / 2
 
             image = image.crop((left_margin, top_margin, right_margin, bottom_margin))
-
         image.save(output_path, "PNG")
 
+@retry_after
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Hi! I can convert and resize pictures in the right size so that you can send them to the sticker bot. I can only convert one image at a time. If you want to convert multiple images quickly then use my other resizer on github (you can find my github on my website)\n\nMade by @binnichtaktiv\nhttps://binnichtaktiv.github.io")
+    bot.reply_to(message, "Hi! I can convert and resize pictures in the right size so that you can send them to the sticker bot. I can only convert one image at a time. If you want to convert multiple images quickly then use my other resizer on github (you can find my github on my website)\n\nMade by @binnichtaktiv\nhttps://binnichtaktiv.github.io")
 
+@retry_after
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
     file_id = message.photo[-1].file_id
@@ -40,4 +55,4 @@ def handle_photo(message):
     os.remove("temp.png")
     os.remove("temp_resized.png")
 
-bot.polling()
+bot.polling(none_stop=True)
