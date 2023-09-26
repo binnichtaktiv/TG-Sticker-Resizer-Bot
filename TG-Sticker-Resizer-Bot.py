1d8c75ea3e0ea44b08042694b8dfab3d156b70e3
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2023-09-26 10:31:52 +0200

    fixed wrong resize and bot timeouts

--- TG-Sticker-Resizer-Bot.py
+++ TG-Sticker-Resizer-Bot.py
@@ -1,58 +1,59 @@
 import telebot
 from PIL import Image
 import os
+import tempfile
 import time
 
-TOKEN = "YOUR_BOT_TOKEN_HERE"
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot("YOUR_BOT_TOKEN")
 
-def retry_after(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except telebot.apihelper.ApiTelegramException as e:
-            if e.error_code == 429:
-                wait_time = e.result_json.get('parameters', {}).get('retry_after', 5)
-                print(f"Too many requests. Waiting {wait_time} seconds.")
-                time.sleep(wait_time)
-                return wrapper(*args, **kwargs)
-            raise e
-    return wrapper
-
-def resize_and_convert_image(input_path, output_path):
+def resize_and_fill_image(input_path, output_path):
     with Image.open(input_path) as image:
-        image.thumbnail((512, 512))
+        target_size = (512, 512)
         
-        if image.size != (512, 512):
-            left_margin = (image.width - 512) / 2
-            top_margin = (image.height - 512) / 2
-            right_margin = (image.width + 512) / 2
-            bottom_margin = (image.height + 512) / 2
+        width_ratio = target_size[0] / image.width
+        height_ratio = target_size[1] / image.height
+        scale_ratio = min(width_ratio, height_ratio)
+        
+        new_width = int(image.width * scale_ratio)
+        new_height = int(image.height * scale_ratio)
+        
+        new_image = Image.new("RGB", target_size, "black")
+        
+        x_offset = (target_size[0] - new_width) // 2
+        y_offset = (target_size[1] - new_height) // 2
+        
+        new_image.paste(image.resize((new_width, new_height), Image.ANTIALIAS), (x_offset, y_offset))
+        
+        new_image.save(output_path, "PNG")
 
-            image = image.crop((left_margin, top_margin, right_margin, bottom_margin))
-        image.save(output_path, "PNG")
-
-@retry_after
 @bot.message_handler(commands=['start'])
-def handle_start(message):
+def send_welcome(message):
     bot.reply_to(message, "Hi! I can convert and resize pictures in the right size so that you can send them to the sticker bot. I can only convert one image at a time. If you want to convert multiple images quickly then use my other resizer on github (you can find my github on my website)\n\nMade by @binnichtaktiv\nhttps://binnichtaktiv.github.io")
 
-@retry_after
 @bot.message_handler(content_types=['photo'])
-def handle_photo(message):
-    file_id = message.photo[-1].file_id
-    file_info = bot.get_file(file_id)
+def handle_image(message):
+    file_info = bot.get_file(message.photo[-1].file_id)
     downloaded_file = bot.download_file(file_info.file_path)
     
-    with open("temp.png", 'wb') as new_file:
-        new_file.write(downloaded_file)
-    
-    resize_and_convert_image("temp.png", "temp_resized.png")
-    
-    with open("temp_resized.png", 'rb') as photo:
-        bot.send_document(message.chat.id, photo)
-    
-    os.remove("temp.png")
-    os.remove("temp_resized.png")
+    with tempfile.TemporaryDirectory() as temp_dir:
+        input_path = os.path.join(temp_dir, 'input.png')
+        output_path = os.path.join(temp_dir, 'output.png')
+        
+        with open(input_path, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        
+        resize_and_fill_image(input_path, output_path)
+        
+        with open(output_path, 'rb') as img:
+            bot.send_document(message.chat.id, img)
 
-bot.polling(none_stop=True)
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, "Error: Please send a picture.")
+
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except Exception as e:
+        print("Bot crashed. Restarting...")
+        time.sleep(10)
